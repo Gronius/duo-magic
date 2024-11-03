c950c958361f7ad7ce9c0295f166511147067b0c
Gronius <vlivonika@gmail.com>
2024-11-03 12:27:09 +0200

    the script is written in a single main file, the extra ones are deleted

--- main.js
+++ main.js
@@ -356,3 +356,36 @@ document.getElementById("livemusic-modall").addEventListener('click', event => {
 
 
 // slider&modals
+
+let slideToShow = 0;
+
+const modalBtns = document.querySelectorAll('.gallery-modal-btn');
+
+modalBtns.forEach(function (button) {
+
+    button.addEventListener('click', function () {
+        const slideNumbeToShow = button.getAttribute('data-index');
+
+        slideToShow = Number(slideNumbeToShow);
+
+        console.log(slideToShow);
+    });
+
+});
+
+// Запуск бібліотеки модального вікна
+MicroModal.init({
+    onShow: function () {
+        slider.go(slideToShow);
+    }
+
+});
+
+
+
+
+// Запуск бібліотеки слайдеру
+
+const sliderContainer = document.querySelector('.splide');
+
+const slider = new Splide(sliderContainer).mount();
